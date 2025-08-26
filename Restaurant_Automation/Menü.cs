bfe9a9751895955f7490f4aab5b1e76d1bab83dc
Yunus Emre <alkanyunusemre2@gmail.com>
2025-08-26 16:48:45 +0300

    Add files via upload

---
+++ Restaurant_Automation/Menü.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Automation
+{
+    internal class Menü
+    {
+
+        internal static List<string> foods = new List<string>() { "Hamburger", "Pizza", "Döner" };
+
+        internal static List<string> drinks = new List<string>() { "Kola", "Soda", "Ayran" };
+
+        internal static List<string> sweets = new List<string>() { "Waffle", "Cheesecake", "Çikolatalı Pasta" };
+
+        internal static List<int> foodsPrice = new List<int>(300 + 250 + 150);
+
+        internal static List<int> drinksPrice = new List<int>() { 70 + 30 + 30 };
+
+        internal static List<int> sweetsPrice = new List<int>() { 300 + 150 + 150 };        
+    
+        
+        internal static List<int> tableNumbers = new List<int>();
+        internal static int masaSayisi = 20;            
+       
+    }
+
+}
---
+++ Restaurant_Automation/Program.cs
@@ -0,0 +1,29 @@
+﻿using Restaurant_Automation;
+
+namespace _Restaurant_Automation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int selectedd;
+            Console.WriteLine("1. Yönetici\n2. Garson");
+            string select = Console.ReadLine()!;
+            
+            InputInt(select, selectedd);
+        }
+        static void InputInt(string select, int selected) 
+        {
+            if (int.TryParse(select, out selected)) 
+            {
+                
+            }
+            else 
+            {
+                Console.WriteLine("HATALI GİRİŞ YATPINIZ!!");
+            }
+
+        }
+
+    }
+}
